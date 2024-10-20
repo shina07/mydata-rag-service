e3aef91a6af9202a195cc62374ec7b4c9aa7e9aa
Taesoo Lim <limtsoo.shina@gmail.com>
2024-10-20 04:02:39 +0000

    [weaviate] volume mount, 환경 변수 추가

--- script/run/run_mrs_weaviate.sh
+++ script/run/run_mrs_weaviate.sh
@@ -9,5 +9,13 @@ docker run -d \
     --name ${MRS_WEAVIATE_CONTAINER_NAME} \
     --network ${MRS_NETWORK}  \
     -p 7070:8080 \
+    -p 50051:50051 \
     -e "PROFILE=dev" \
+    -e "QUERY_DEFAULTS_LIMIT=25" \
+    -e "AUTHENTICATION_ANANYMOUS_ACCESS_ENABLED=true" \
+    -e "PERSISTENCE_DATA_PATH=/var/lib/weaviate" \
+    -e "DEFAULT_VECTORIZER_MODULE=none" \
+    -e "ENABLE_API_BASED_MODULES=true" \
+    -e "CLUSTER_HOSTNAME=mrs-weaviate-master" \
+    -v ${MRS_DATA}/${MRS_WEAVIATE_CONTAINER_NAME}:/var/lib/weaviate \
     ${MRS_WEAVIATE_CONTAINER_NAME}:${MRS_WEAVIATE_TAG}
